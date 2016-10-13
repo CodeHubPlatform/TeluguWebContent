54da61135a7536bd34ccc12cc2a21fc3086a0fcc
parth <prashanth726@gmail.com>
2016-10-14 02:55:16 +0530

    added service worker;

--- sw.js
+++ sw.js
@@ -1,39 +1,34 @@
-// serviceWorker.js
----
-layout: null
----
-importScripts('js/vendor/serviceworker-cache-polyfill.js');
-
-var cacheName = 'eduardoboucas.com-cache-v1';
-var filesToCache = ['/css/main.css',
-    {% for page in site.html_pages %}
-      '{{ page.url }}',
-    {% endfor %}{% for post in site.posts %}
-      '{{ post.url }}',
-  {% endfor %}
+importScripts('https://codehubplatform.github.io/EnglishWebContent/cache-polyfill.js');
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  
+  'index.html',
+  '/EnglishWebContent//2015/01/02/internet.html',
+  'https://codehubplatform.github.io/EnglishWebContent//2015/01/02/internet.html'
 ];
 
-// serviceWorker.js
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    console.log('[*] Serving cached: ' + event.request.url);
-                    return response;
-                }
-
-                console.log('[*] Fetching: ' + event.request.url);
-                return fetch(event.request);
-            }
-        )
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        console.log('resoincesis');
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });

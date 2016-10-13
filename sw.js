6d8801a808ff512c97ea87a569878b48f951abd7
parth <prashanth726@gmail.com>
2016-10-14 03:29:00 +0530

    service worker;

--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
 importScripts('https://codehubplatform.github.io/EnglishWebContent/cache-polyfill.js');
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  
+  '/EnglishWebContent/css/main.css',
+  '/EnglishWebContent/javascript/marksheet.js',
   'index.html',
-  {% for post in site.posts %}
-      '{{ post.url }}',
-  {% endfor %}
+  '/EnglishWebContent//2015/01/01/introduction.html',
+  '/EnglishWebContent//2015/01/02/internet.html',
+  '/EnglishWebContent//2015/01/04/browser.html'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        console.log('resoincesis');
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    console.log('[*] Serving cached: ' + event.request.url);
+                    return response;
+                }
+
+                console.log('[*] Fetching: ' + event.request.url);
+                return fetch(event.request);
+            }
+        )
+    );
 });
